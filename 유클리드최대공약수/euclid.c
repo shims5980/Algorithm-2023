c372dee7d46e0d0e82cbd26f1be6ea111d181282
19_심기윤 <127172908+shims5980@users.noreply.github.com>
2023-03-20 13:37:52 +0900

    Update euclid.c

--- 유클리드최대공약수/euclid.c
+++ 유클리드최대공약수/euclid.c
@@ -1,1 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int Euclid(int a, int b);
+int test1_euclid()
+{
+	int gcd;
+
+	gcd = Euclid(24, 14);
+
+	printf("%d", gcd);
+
+	return 0;
+}
+int main(void)
+{
+	test1_euclid();
+}
+int Euclid(int a, int b)
+{
+	
+	//1. If(b == 0) return a
+	if (b == 0)
+		return a;
+
+	//2. return Euclid(b, a mod b)
+	return Euclid(b, a % b);
+}

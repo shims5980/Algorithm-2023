7a17cb343dbb057bbce55760a8fd2cced026cb75
19_심기윤 <127172908+shims5980@users.noreply.github.com>
2023-03-22 18:31:47 +0900

    Update Algorithm-2023.c

--- Algorithm-2023/Algorithm-2023.c
+++ Algorithm-2023/Algorithm-2023.c
@@ -1,4 +1,8 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+/*  실습 내용: Hello, world 출력하기
+    실습 날짜: 2023.03.07
+    학번: 201910852
+    이름: 심기윤 */  
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)

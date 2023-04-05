5c3ece7007f3f1bc1c3882370ee28c6ecc4b19de
19_심기윤 <127172908+shims5980@users.noreply.github.com>
2023-04-05 14:28:38 +0900

    Update 2번.c

--- chap1/임의의 숫자 찾기/2번.c
+++ chap1/임의의 숫자 찾기/2번.c
@@ -1,25 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int findRandomNumber(int array[], int userWant);
+void test_findRandomNumber();
+
 int main() {
+    
+    test_findRandomNumber();
+
+    return 0;
+}
+
+void test_findRandomNumber()
+{
     int cards[] = { 45, 20, 60, 35, 10, 90, 85, 75, 25 };
-    int target = 85;
+    int target = 85, index;
+
+
+    index = findRandomNumber(cards, target);
+
+    if (index == -1)
+        printf("찾고자 하는 값이 cards배열안에 없음\n");
+    else
+        printf("%d가 cards[%d]에 있음\n", target, index);
+}
+
+int findRandomNumber(int array[], int wantFind)
+{
     int i, index = -1;
 
-    // 배열을 처음부터 끝까지 탐색하며 target 값이 있는 인덱스를 찾음
+    // 배열을 처음부터 끝까지 탐색하며 원하는 값이 있는 인덱스를 찾음
     for (i = 0; i < 9; i++) {
-        if (cards[i] == target) {
+        if (array[i] == wantFind) {
             index = i;
             break;
         }
     }
 
-    // target 값이 있는 인덱스를 출력
-    if (index == -1) {
-        printf("찾는 값이 85가 배열에 없습니다.");
-    }
-    else {
-        printf("85는 %d번째에 있습니다", index);
-    }
-
-    return 0;
+    //원하는 값이 있는 인덱스를 반환
+    return index;
 }

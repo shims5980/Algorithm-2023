2fedf2a8ce84d3ec777b7306ad704bdc2e8b8dd4
19_심기윤 <127172908+shims5980@users.noreply.github.com>
2023-04-10 16:36:34 +0900

    Create main.c

---
+++ chap2/유클리드 최대공약수/main.c
@@ -0,0 +1,20 @@
+/*	과목: 알고리즘
+	이름: 심기윤
+	학번: 201910852
+	실습날짜: 2023.
+	실습내용: 	*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int testEuclid();
+void VtestEuclid();
+
+int main(void)
+{
+	int ret = 0;
+	
+	ret = testEuclid();
+	VtestEuclid();
+
+	return 0;
+}

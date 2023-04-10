169adecded81b90670f8363a3f74729106776e79
19_심기윤 <127172908+shims5980@users.noreply.github.com>
2023-04-10 16:37:44 +0900

    Create euclid.c

---
+++ chap2/유클리드 최대공약수/euclid.c
@@ -0,0 +1,37 @@
+//유클리드 최대공약수 재귀호출 방법
+int euclidRecursive(int a, int b)
+{
+	//1. If(b == 0) return a
+	if (b == 0)
+		return a;
+
+	//2. return Euclid(b, a mod b)
+	return euclidRecursive(b, a % b);
+}
+
+//유클리드 최대공약수 반복적인 방법
+int euclidRepeative(int a, int b)
+{
+	//1. IF a < b RETURN 0; // 입력확인
+	if (a < b)
+		return 0;
+
+	//2. INT bsav; // 임시 기억
+	int temp = 0;
+
+	//3. WHILE(b != 0)
+	//	4. {
+	//	bsav = b
+	//		b = a mod b
+	//		a = bsav
+	//}
+	while (b != 0)
+	{
+		temp = b;
+		b = a % b;
+		a = temp;
+	}
+	
+	//5. RETURN a
+	return a;
+}

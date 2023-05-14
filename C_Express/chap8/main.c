2d7055cd55c5bbd6cd8f6ee6e2e71eabf5036b62
19_심기윤 <127172908+shims5980@users.noreply.github.com>
2023-05-14 17:09:30 +0900

    Create main.c

---
+++ C_Express/chap8/main.c
@@ -0,0 +1,14 @@
+/*	과목: 알고리즘
+	이름: 심기윤
+	학번: 201910852
+	실습날짜: 2023. 5.9
+	실습내용: 이진 트리	*/
+
+extern void test_tree1();
+extern void test_tree2();
+
+void main()
+{
+	test_tree1();
+	test_tree2();
+}
